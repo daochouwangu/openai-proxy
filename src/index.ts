addd6d7340d8642ec9ff1c7dc213f3672b0de31a
TimLi <daochouwangu@gmail.com>
2024-06-25 15:33:31 +0800

    禁止香港服务器

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,28 @@ const handleRequest = async (request: Request): Promise<Response> => {
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
+// https://community.openai.com/t/api-access-failed-showing-cloudflare-block-page/506015/22
+export const config = {
+  runtime: "edge",
+  // https://vercel.com/docs/edge-network/regions#region-list
+  regions: [
+    "arn1",
+    "bom1",
+    "cdg1",
+    "cle1",
+    "cpt1",
+    "dub1",
+    "fra1",
+    "gru1",
+    // 'hkg1', ip blocked by openai
+    "hnd1",
+    "iad1",
+    "icn1",
+    "kix1",
+    "lhr1",
+    "pdx1",
+    "sfo1",
+    "sin1",
+    "syd1",
+  ],
+};
